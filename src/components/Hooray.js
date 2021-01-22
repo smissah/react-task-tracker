e55de9fde891a3df0a37a4f86549fadbd6437d90
smissah <stephenmissah@outlook.com>
2021-01-22 11:31:15 +0000

    removed manifest from index.html, added hooray component and jsondb

---
+++ src/components/Hooray.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hooray = () => {
+  return (
+    <div>
+      <h3 className="completed">Hooray, all tasks completed!</h3>
+    </div>
+  );
+};
+
+export default Hooray;
